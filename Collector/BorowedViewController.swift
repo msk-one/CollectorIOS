a8d80e9e82b5e300e6c4df4bccb768aa02a21aaf
Michal Szklarski <msk@nex10.pl>
2016-02-29 20:06:19 +0100

    Second init.

---
+++ Collector/BorowedViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Collector
+//
+//  Created by Msk Sk on 03/01/16.
+//  Copyright © 2016 Msk. All rights reserved.
+//
+
+import UIKit
+
+class BorowedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
